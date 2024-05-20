d54bf1995453cc28d7e1d02a6744e6bad443796d
Steven <brookscodeblog@gmail.com>
2024-05-20 11:25:56 +0100

    build hambuger menu and make header responsive

---
+++ app/public/wp-content/themes/earbuddy/assets/js/dropdown-toggle.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const overlay = document.querySelector('.overlay');
+    const dropdown = document.querySelector('.dropdown');
+    const bar1 = document.querySelector('.bar1');
+    const bar2 = document.querySelector('.bar2');
+    const bar3 = document.querySelector('.bar3');
+
+    hamburger.addEventListener('click', () => {
+        overlay.classList.toggle('show-overlay');
+        dropdown.classList.toggle('show-dropdown');
+        bar1.classList.toggle('bar1-dropdown');
+        bar2.classList.toggle('bar2-dropdown');
+        bar3.classList.toggle('bar3-dropdown');
+    });
+
+});
--- app/public/wp-content/themes/earbuddy/header.php
+++ app/public/wp-content/themes/earbuddy/header.php
@@ -18,6 +18,18 @@
             <nav class="main-nav">
                 <?php wp_nav_menu(array('theme_location' => 'earbuddy_main_menu', 'depth' => 2)); ?>
             </nav>
+            <nav class="dropdown-nav">
+                <div class="overlay"></div>
+                <button class="hamburger">
+                    <div class="bar bar1"></div>
+                    <div class="bar bar2"></div>
+                    <div class="bar bar3"></div>
+                </button>
+                <div class="dropdown">
+                    <?php wp_nav_menu(array('theme_location' => 'earbuddy_main_menu', 'depth' => 2)); ?>
+                    <?php wp_nav_menu(array('theme_location' => 'earbuddy_bottom_menu', 'depth' => 2)); ?>
+                </div>
+            </nav>
         </div>
         <div class="bottom-bar">
             <nav class="bottom-nav">
